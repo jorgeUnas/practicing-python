807c3a2262daa4a4a6f02a48c37cfddeee6c8b36
EC2 Default User <ec2-user@ip-172-31-0-26.eu-north-1.compute.internal>
2023-12-11 18:55:57 +0000

    Playing with integers and floats

---
+++ basics/.~c9_invoke_eWof97.py
@@ -0,0 +1,17 @@
+a = 5
+b = 10.67
+c = 'Hello World'
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(isinstance(b, float))
+print(isinstance(a, float))
+
+print(a)
+print(float(a))
+print(a)
+print()
