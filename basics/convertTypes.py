196054c78164c910f0b9261191d760d7cd79387d
EC2 Default User <ec2-user@ip-172-31-0-26.eu-north-1.compute.internal>
2023-12-12 19:14:41 +0000

    playing with data types conversions

---
+++ basics/convertTypes.py
@@ -0,0 +1,11 @@
+number1 = 99.999
+
+print(int(number1)) #cut the number
+print(str(number1))
+print(number1 + 23)
+# print(str(number1) + 23)  syntax error
+
+number1 = 'ninety-nine'
+
+# print(int(number1)) error 
+
