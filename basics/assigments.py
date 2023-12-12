d8ae23d07492bf4f649317dbb399bfc9bf7af473
EC2 Default User <ec2-user@ip-172-31-0-26.eu-north-1.compute.internal>
2023-12-12 18:46:31 +0000

    Multiple statements

---
+++ basics/assigments.py
@@ -0,0 +1,12 @@
+# multiple assigments
+
+first, second, third = "Rosa", "Jorge", "Lopez" # n names must have 3 values
+
+print(F'The participants are {first}, {second} and {third}')
+
+# assigments with key, values
+
+members = {"Rob": 21, "John": 50, "Mel": 34}
+
+for key, value in members.items():
+    print(F'{key} is {value} years old.')
