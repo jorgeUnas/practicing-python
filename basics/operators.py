492092a562cd789635d5550ea4930df2ddb2c81f
jorgeUnas <>
2023-12-13 19:37:25 +0000

    comparison adn logical operators

--- basics/operators.py
+++ basics/operators.py
@@ -30,3 +30,24 @@ print(int(z)) #division converts to float
 
 z **= b
 print(z)
+
+# comparison operators 
+
+x = 100 
+y = 500
+w = 100
+
+print(x == y)
+print(x != w)
+print(x > y)
+print(x < y)
+print(x >= w)
+print(x <= y)
+
+# Logical operators
+
+a = 4 
+
+print(a > 2 and a < 9)
+print(a > 3 or a == 5)
+print(not(a > 2 and a < 9))
