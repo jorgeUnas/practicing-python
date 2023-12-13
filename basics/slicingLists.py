b99feeac7834e47add087b85f06928b43bd53551
jorgeUnas <>
2023-12-13 18:31:05 +0000

    slicing lists

---
+++ basics/slicingLists.py
@@ -0,0 +1,13 @@
+newList = ['rob', 'maria', 'carlos', 'petro', 'mike', 'pete']
+print(newList[:3])
+print(newList[1:4])
+
+for name in newList[3:]:
+    print(name.title())
+
+# including increment
+
+nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+print(nums[1:11:2])
+
