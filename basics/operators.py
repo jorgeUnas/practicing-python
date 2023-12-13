898c64a8db781500f1b9e66ce09dedfec18bbd9a
jorgeUnas <>
2023-12-13 19:12:21 +0000

    aritmetic and assignment operators

---
+++ basics/operators.py
@@ -0,0 +1,32 @@
+# aritmetic operators
+
+a = 5
+b = 2
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a ** b
+i = a % b
+
+print([c, d, e, f, g, i])
+
+#assignment operators
+
+z = 10
+
+z += b
+print(z)
+
+z -= b
+print(z)
+
+z *= a
+print(z)
+
+z /= b
+print(int(z)) #division converts to float
+
+z **= b
+print(z)
