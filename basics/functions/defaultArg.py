a9dacf069b8036a30b742989ed6d4e072e6df92f
jorgeUnas <>
2023-12-22 16:34:29 +0000

    default arguments

---
+++ basics/functions/defaultArg.py
@@ -0,0 +1,8 @@
+def professional(name, lastName = 'Fulano', profession = 'chemist'):
+    print(name, lastName, 'is a', profession)
+
+professional('Jorge', 'Unas')
+
+professional('Hulk')
+
+professional('Tony', 'Stark', 'Physics')
