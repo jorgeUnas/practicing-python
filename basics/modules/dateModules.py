8d8efaf35ffe331052aff197b94a45e4abcf14e3
EC2 Default User <ec2-user@ip-172-31-0-26.eu-north-1.compute.internal>
2023-12-25 17:09:59 +0000

    using date and datetime modules

---
+++ basics/modules/dateModules.py
@@ -0,0 +1,9 @@
+from datetime import date
+from datetime import time
+from datetime import datetime
+
+today = date.today()
+time_now = datetime.now()
+
+print('Today date is', today)
+print('The exact time is', time_now)
